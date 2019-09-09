013d6a602c2faf880b55118b7d8ad9cee583ca8c
Ryan Keys <r.keys1998@gmail.com>
2019-09-09 15:18:00 -0700

    Create tenor.py

---
+++ tenor.py
@@ -0,0 +1,22 @@
+import requests
+import json
+# set the apikey and limit
+apikey = "CQ58OJ4GZ9WQ"  # test value
+lmt = 8
+
+# our test search
+search_term = "excited"
+
+# get the top 8 GIFs for the search term
+r = requests.get(
+    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+
+if r.status_code == 200:
+    # load the GIFs using the urls for the smaller GIF sizes
+    top_8gifs = json.loads(r.content)
+    print(top_8gifs)
+else:
+    top_8gifs = None
+
+
+# continue a similar pattern until the user makes a selection or starts a new search.
