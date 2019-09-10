cff9fc8449956bc86e56bb158d7b3062e4716677
Ben <benlaugherty@gmail.com>
2019-09-09 21:51:31 -0700

    [add] basic gif search functionailty with the framework for adding popular searches, ready for styling

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from tenor import gif_search, get_popular_gifs
 
 app = Flask(__name__)
 
 
 @app.route('/')
+# @app.route('/search', methods=['GET', 'POST'])
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
+    display_gifs = True
 
-    # TODO: Make an API call to Tenor using the 'requests' library
+    query = request.args.get('query')
+    results = gif_search(query)['results']
 
-    # TODO: Get the first 10 results from the search results
+    gifs = []
+    for gif in results:
+        gifs.append(gif)
 
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    if not gifs:
+        display_gifs = False
 
-    return render_template("index.html")
+    return render_template('index.html', gifs=gifs, display=display_gifs)
+
+    # popular_gifs = get_popular_gifs()['results']
+    # gifs = []
+    # for gif in popular_gifs:
+    #     gifs.append(gif)
+    # print('rendering most popular gifs!')
+    # return render_template('index.html', gifs=gifs)
 
 
 if __name__ == '__main__':
--- tenor.py
+++ tenor.py
@@ -1,27 +1,28 @@
 import requests
 import json
-# set the apikey and limit
+# set the apikey
 apikey = "CQ58OJ4GZ9WQ"
-lmt = int
-
-# our test search
-search_term = " "
+url = "https://api.tenor.com/v1"
 
 # get the top 8 GIFs for the search term
 
 
-def gif_search(lmt):
-    r = requests.get(
-        "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, apikey, lmt))
+def gif_search(query):
+    r = requests.get(url + "/search?q=%s&key=%s&limit=10" % (query, apikey))
 
     if r.status_code == 200:
         # load the GIFs using the urls for the smaller GIF sizes
-        top_gifs = json.loads(r.content)
-        print(top_gifs)
+        gifs = json.loads(r.content)
+        return gifs
     else:
-        top_gifs = None
+        return None
 
 
-lmt = int(input("Enter a number of GIFS to search"))
-search_term = str(input("Enter a keyword for your search"))
-gif_search(lmt)
+def get_popular_gifs():
+    r = requests.get(url + "/trending?key=%s&limit=10" % (apikey))
+
+    if r.status_code == 200:
+        trending_gifs = json.loads(r.content)
+        return trending_gifs
+    else:
+        return None
